fffd1f63c557ee7bcb4323805263d55e4520c052
LeoM320 <meolansl@fcal.uner.edu.ar>
2026-05-14 17:30:16 -0300

    feat(firmware): inicializar proyecto base de Microchip Studio
    - Actualizar reglas del .gitignore para AVR GCC, Microchip y Qt
    - Agregar solución (.atsln) y configuración (.cproj) del proyecto
    - Crear archivo main.c inicial

---
+++ cinta-clasificadora/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 5/14/2026 5:20:56 PM
+ *  Author: meola
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
